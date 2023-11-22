86359a8037c5b926c718472127f9e5aafb2f2210
saganorth <saga.nord99@hotmail.se>
2023-11-22 20:56:18 +0100

    first commit

---
+++ routes/besokare.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { loadData, saveData } from '../dataHandler.js';  
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    try {
+        const newVisitor = req.body;
+        const data = loadData();
+
+        if (!data.besokare || !Array.isArray(data.besokare)) {
+            data.besokare = [];
+        }
+
+        data.besokare.push(newVisitor);
+        saveData(data);
+
+        res.json({ message: 'Data mottagen och sparad!', updatedData: data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Ett internt serverfel inträffade');
+    }
+});
+
+router.get('/', (req, res) => {
+    try {
+        const data = loadData();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Ett internt serverfel inträffade');
+    }
+});
+export default router;
